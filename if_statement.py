2ac132ac841861c0f53345bff1dc3a6c13bf28c9
Asif <asifhayatanjon@gmail.com>
2022-04-05 10:58:54 +0600

    new commit

---
+++ if_statement.py
@@ -0,0 +1,9 @@
+val1=int(input('Enter the first number: '))
+val2=int(input('Enter the second number: '))
+
+if val1>val2:
+    print(val1, " is greater than ", val2)
+elif val1<val2:
+    print(val1, "is less than ", val2)
+else:
+    print("both are equal")
