86c03c1513ce8dea05f012d1d0652b269825c9b2
Asif <asifhayatanjon@gmail.com>
2022-04-06 09:48:17 +0600

    first commit

---
+++ dic.py
@@ -0,0 +1,13 @@
+my_dict={
+    'name': 'Asif',
+    'age': 24,
+    'degree': 'BSC',
+    'Nationality': 'Banladeshi',
+    'Language': ['Bangla','English']
+
+
+}
+print(my_dict)
+print(my_dict['name'])
+print(my_dict['Language'])
+print(my_dict['Language'][0])
