c1ed478b53ec4ece7f2845aafcda544156feb5bf
Asif <asifhayatanjon@gmail.com>
2022-04-08 10:31:15 +0600

    new commit

---
+++ from_in.py
@@ -0,0 +1,4 @@
+class Python():
+    name="asif"
+    age=98
+    id=1034
---
+++ inhert.py
@@ -0,0 +1,8 @@
+from from_in import Python
+class Devid(Python):
+    pass
+
+obj=Devid()
+print(obj.name)
+print(obj.age)
+
